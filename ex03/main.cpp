baa2aabd693b08d4502227ea0ee9d7f674d514a9
0yech <Nelson.rey02@gmail.com>
2025-09-25 02:28:44 +0200

    slight changes

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nrey <nrey@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 03:29:52 by nrey              #+#    #+#             */
-/*   Updated: 2025/06/07 20:11:52 by nrey             ###   ########.fr       */
+/*   Updated: 2025/09/25 02:24:56 by nrey             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 int main()
 {
     DiamondTrap d1("Dia1");
+    DiamondTrap d2("JAKS");
+    DiamondTrap d3(d2);
+
+    d3 = d2;
     d1.attack("chill valley");
     d1.whoAmI();
 
